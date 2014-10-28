b14cf62c9196c4ce368800f71971e2e2afa8757d
polle-vanhoof <pello_ski@hotmail.com>
2014-10-28 13:34:11 +0100

    fixed main

--- src/client/main.java
+++ src/client/main.java
@@ -1,16 +1,22 @@
 package client;
 
 import java.util.Date;
-import javax.naming.InitialContext;
-import rental.CarType;
+import java.util.List;
+import java.util.Set;
 
-public class Main extends AbstractScriptedSimpleTripTest<CarRentalSessionRemote, CarRentalManagerRemote>{
+import javax.naming.InitialContext;
+
+import rental.CarType;
+import rental.Reservation;
+import session.CarRentalSessionRemote;
+import session.CarRentalManagerRemote;
+
+public class Main extends AbstractScriptedTripTest<CarRentalSessionRemote, CarRentalManagerRemote>{
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        System.out.println("found rental companies: "+session.getAllRentalCompanies());
         Main main = new Main("simpleTrips");
         main.run();
     }
@@ -28,30 +34,10 @@ public class Main extends AbstractScriptedSimpleTripTest<CarRentalSessionRemote,
     }
 
     @Override
-    protected CarRentalManagerRemote getNewManagerSession(String name, String carRentalName) throws Exception {
-        InitialContext context = new InitialContext();
-        CarRentalManagerRemote managerSession = (CarRentalManagerRemote) context.lookup(CarRentalManagerRemote.class.getName());
-        managerSession.setClientName(name);
-        managerSession.setCompanyName(carRentalName);
-        return managerSession;
-    }
-
-    @Override
     protected void checkForAvailableCarTypes(CarRentalSessionRemote session, Date start, Date end) throws Exception {
         for(CarType carType : session.getAvailableCarTypes(start, end)){
             System.out.println(carType.toString());
         }
-    }
-
-    @Override
-    protected void addQuoteToSession(CarRentalSessionRemote session, String name, Date start, Date end, String carType, String carRentalName) throws Exception {
-        ReservationConstraints constraints = new ReservationConstraints(start, end, carType);
-        session.createQuote(constraints, carRentalName);
-    }
-
-    @Override
-    protected void confirmQuotes(CarRentalSessionRemote session, String name) throws Exception {
-        session.confirmQuotes();
     }
 
     @Override
@@ -63,4 +49,40 @@ public class Main extends AbstractScriptedSimpleTripTest<CarRentalSessionRemote,
     protected int getNumberOfReservationsForCarType(CarRentalManagerRemote ms, String carRentalName, String carType) throws Exception {
         return ms.getNbOfReservationsByCarType(carRentalName, carType);
     }
+
+	@Override
+	protected CarRentalManagerRemote getNewManagerSession(String name) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getCheapestCarType(CarRentalSessionRemote session, Date start, Date end) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void addQuoteToSession(CarRentalSessionRemote session, Date start, Date end, String carType, String carRentalName) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected List<Reservation> confirmQuotes(CarRentalSessionRemote session) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Set<String> getBestClients(CarRentalManagerRemote ms) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected CarType getMostPopularCarTypeIn(CarRentalManagerRemote ms, String carRentalCompanyName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

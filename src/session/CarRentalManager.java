c76ce091b581d7a94eb5489ed1b2dad3f938f00e
polle-vanhoof <pello_ski@hotmail.com>
2014-10-28 14:50:42 +0100

    test comit

--- src/session/CarRentalManager.java
+++ src/session/CarRentalManager.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package session;
 
 import java.rmi.RemoteException;
@@ -86,7 +81,7 @@ public class CarRentalManager implements CarRentalManagerRemote {
     }
     
     public void registerCompany(CarRentalCompanyRemote company){
-    	// TODO
+    	RentalStore.addRentalCompany(this.companyName, company);
     }
 
 }
